a86b52c70ca648c6ed4a13746977763c8e0e4fd0
dhondiram.jawade <dhondiram.jawade@innoeye.com>
2020-11-28 02:03:43 +0530

    side nav

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  typesOfShoes: string[] = ['Peacock', 'Dove', 'Sparrow', 'Goose', 'Ostrich', 'Pigeon', 'Turkey', 'Hawk', 'Bald eagle', 'Raven', 'Parrot'];
 }
--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule, DrawerModule, IconModule, InputModule, ToolbarModule } from "@gx/ui-tools";
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, InputModule, DrawerModule, ToolbarModule, ButtonModule, IconModule],
+  imports: [BrowserModule, BrowserAnimationsModule, InputModule, DrawerModule, ToolbarModule, ButtonModule, IconModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
