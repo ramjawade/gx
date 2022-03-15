efbb6ce90b16f47be27c66e3a645fbafed9452d2
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2022-03-15 23:18:38 +0530

    pwa

--- apps/vachanalay/src/app/app.module.ts
+++ apps/vachanalay/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from "@angular/material/icon";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -14,6 +16,12 @@ import { MatIconModule } from "@angular/material/icon";
     FlexLayoutModule,
     MatIconModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
